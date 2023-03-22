03114d6fab357ed1fc9ee9fff14b390e43f4ee8c
TorresHub9710 <heortiz34510@universidadean.edu.co>
2023-03-22 06:54:28 -0500

    update guia 3 y guia 4

---
+++ Guia 3/Actividad 3/EvaluadorPostfijo/src/universidadean/desarrollosw/postfijo/EvaluadorPostfijo.java
@@ -0,0 +1,81 @@
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Universidad Ean (Bogotá - Colombia)
+ * Departamento de Tecnologías de la Información y Comunicaciones
+ * Licenciado bajo el esquema Academic Free License version 2.1
+ * <p>
+ * Proyecto Evaluador de Expresiones Postfijas
+ * Fecha: Febrero 2021
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+package universidadean.desarrollosw.postfijo;
+
+import java.io.IOException;
+import java.io.StreamTokenizer;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Esta clase representa una clase que evalúa expresiones en notación polaca o
+ * postfija. Por ejemplo: 4 5 +
+ */
+public class EvaluadorPostfijo {
+
+    /**
+     * Realiza la evaluación de la expresión postfijo utilizando una pila
+     * @param expresion una lista de elementos con números u operadores
+     * @return el resultado de la evaluación de la expresión.
+     */
+    static int evaluarPostFija(List<String> expresion) {
+        Stack<Integer> pila = new Stack<>();
+
+        // TODO: Realiza la evaluación de la expresión en formato postfijo
+        int numero, a, b, resultado = 0;
+        for (int i = 0; i < expresion.size(); i++){
+            try {
+                numero = Integer.parseInt(expresion.get(i));
+                pila.push(numero);
+            } catch (NumberFormatException exception){
+                a = pila.pop();
+                b = pila.pop();
+                if (expresion.get(i).equals("+")){
+                    resultado = b + a;
+                    pila.push(resultado);
+                } else if (expresion.get(i).equals("-")) {
+                   resultado = b - a;
+                   pila.push(resultado);
+                } else if (expresion.get(i).equals("*")) {
+                    resultado = b * a;
+                    pila.push(resultado);
+                } else if (expresion.get(i).equals("/")) {
+                    resultado = b / a;
+                    pila.push(resultado);
+                }
+            }
+        }
+
+        return pila.pop();
+    }
+
+    /**
+     * Programa principal
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("> ");
+        String linea = teclado.nextLine();
+
+        try {
+            List<String> expresion = Token.dividir(linea);
+            System.out.println(evaluarPostFija(expresion));
+        }
+        catch (Exception e) {
+            System.err.printf("Error grave en la expresión: %s", e.getMessage());
+        }
+
+    }
+}
